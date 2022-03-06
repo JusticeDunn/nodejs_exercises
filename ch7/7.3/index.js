3561d08c8e43da7e67086cef1fbb346d5fb1ae21
Justice Dunn <justicedunn@me.com>
2022-03-06 13:26:44 -0700

    Implemented 7.3

---
+++ ch7/7.3/index.js
@@ -0,0 +1,23 @@
+class Queue {
+    constructor(executor) {
+        this.queue = [];
+
+        function enqueue(item) {
+            const resolve = this.pending.shift();
+            if (resolve) {
+                return resolve(item);
+            }
+            this.queue.push(item)
+        }
+        executor(enqueue.bind(this));
+    }
+
+    dequeue() {
+        return new Promise((resolve) => {
+            if (this.queue.length === 0) {
+                return this.pending.push(resolve);
+            }
+            resolve(this.queue.shift());
+        });
+    }
+}
